a55c90d50853d68c5cf257225daa96f13be33b6b
Mythili S <indira.mythili@gmail.com>
2021-08-05 00:38:58 +0530

    Added syles to message text and time stamps

--- client/script.js
+++ client/script.js
@@ -1,59 +1,75 @@
-const socket = io();
+// const socket = io();
+const socket = io("http://localhost:6001");
+
+// Get a hold of necessary UI elements
 const messages = document.getElementById('messages');
-// const moment = require("moment");
-// const userName = prompt("Recognize yourself!");
-// socket.emit('new-user', userName);
+let msgInput = document.getElementById("messages");
+let typing = document.getElementById("typing");
 
-(function () {
-    $("form").submit(function (win) {
-        let list = document.createElement("li");
-        // Preventing window reloading
-        win.preventDefault();
-       /*  socket.on('user-connected', userName => {
-        console.log("file: script.js ~ line 13 ~ userName", userName)
-            messages.appendChild(list).append(`${userName} connected`);
-        }); */
-        socket.emit("sendMessage", $("input").val());
+const userName_value = prompt("Recognize yourself!");
+const userColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
 
-        messages.appendChild(list).append($("input").val());
-        let span = document.createElement("span");
-        messages.appendChild(span).append("by UserA: just now");
+let userInfo = {
+    userName: userName_value,
+    profileColor: userColor
+}
 
-        $("input").val("");
-        return false;
-    });
+function setCSSStyles(list, span, data) {
+    list.style.marginTop = "20px";
+    list.style.borderRadius = "10px";
+    list.style.overflowWrap = "break-word";
+    
+    span.style.fontSize = "1.2rem";
+    if (data)
+        span.style.color = data.profileColor;
+}
 
-    socket.on("msgReceived", data => {
-        console.log("Lets check the data: ", data);
-        let list = document.createElement("li");
-        let span = document.createElement("span");
-        let messages = document.getElementById("messages");
-        messages.appendChild(list).append(data.message);
-        messages.appendChild(span).append("by UserB: just now");
-    });
-})();
-
-// Fetch the history of chat messages from db
-const fetchMsgs = function () {
+/*
+ * Fetch the history of chat messages from db
+ */
+function fetchMsgs(name) {
     fetch("/chats").then(data => {
-        console.log("file: script.js ~ line 32 ~ fetch ~ data", data)
         return data.json();
     }).then(jsonData => {
         jsonData.map(msg => {
             let li = document.createElement("li");
             let span = document.createElement("span");
-            messages.appendChild(li).append(msg.message);
+
+            // Adding styles
+            setCSSStyles(li, span);
+            messages
+                .appendChild(li)
+                .append(msg.senderId + " : " + msg.message);
             messages
                 .appendChild(span)
-                .append("by " + msg.senderId + ": " + formatTimeAgo(msg.createdAt));
+                .append(moment(msg.createdAt).fromNow());
         });
+    }).catch(err => {
+        console.error(err);
+        alert("Connection failed ", err);
     });
 };
 
-fetchMsgs();
+// Attach event listeners for UI elements
+const input_var_ID = "input"
 
-let msgInput = document.getElementById("messages");
-let typingStatus = document.getElementById("typingstatus");
+$("form").submit(function (windowRef) {
+    // Preventing window reloading
+    windowRef.preventDefault();
+
+    var dataJSON = {
+        'message': $(input_var_ID).val(),
+        'username': userName_value,
+        'sendDate': Date.now(),
+        'profileColor': userColor,
+    };
+    $(input_var_ID).val("");
+    // When someone sends any message
+    socket.emit("sendMessage", JSON.stringify(dataJSON));
+    populateUI(dataJSON);
+
+    return false;
+});
 
 msgInput.addEventListener("keypress", () => {
     socket.emit("typing", {
@@ -62,16 +78,47 @@ msgInput.addEventListener("keypress", () => {
     });
 });
 
-socket.on("notifyTyping", data => {
-    console.log("data: ", data);
-    typingStatus.innerText = data.user + " " + data.message;
-});
-
-//stop typing
 msgInput.addEventListener("keyup", () => {
     socket.emit("stopTyping", "");
+});
+
+// Attach Socket events
+socket.on("notifyTyping", data => {
+    console.log("data: ", data);
+    typing.innerText = data.user + " " + data.message;
 });
 
 socket.on("notifyStopTyping", () => {
     typing.innerText = "";
 });
+
+socket.on('user-connected', userInfo => {
+    $("#userId").text(userInfo.userName);
+    $("#userId").css("color", userInfo.profileColor);
+});
+
+function populateUI(dataJSON) {
+    let list = document.createElement("li");
+    let span = document.createElement("span");
+    let messages = document.getElementById("messages");
+
+    // Setting css properties
+    setCSSStyles(list, span, dataJSON);
+
+    messages.appendChild(list).append(dataJSON.username + " : " + dataJSON.message);
+    messages.appendChild(span).append(moment(dataJSON.sendDate).fromNow());
+}
+
+socket.on('sendMessage', data => {
+    console.log("SendMessage Lets check the data: ", data);
+    populateUI(data);
+});
+
+socket.on("msgReceived", data => {
+    console.log("msgReceived Lets check the data: ", data);
+    populateUI(data);
+});
+socket.emit('new-user', userInfo);
+
+// -------------   Invoke necessary functions ----------
+fetchMsgs();
