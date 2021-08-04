2a523e5491ec7f88ef860d6194c48761de5dbd62
Mythili S <mythili.s@ril.com>
2021-08-05 00:28:37 +0530

    Added socket events and exchanging user info

--- index.js
+++ index.js
@@ -8,73 +8,79 @@ const chatRouter = require("./routes/chats.js");
 const http = require('http').createServer(app);
 const io = require("socket.io");
 
-
-// Middleware for parsing json in body payload
-app.use(bodyParser.json());
-
-// Add routes
-app.use("/chats", chatRouter);
-
-// Set the express.static middleware
-app.use(express.static(__dirname + "/client"));
-
+const moment = require("moment")
 // Integrating socketio
 const socket = io(http);
 // Db connection
 const Chat = require('./chats.schema.js');
 const dbConnection = require('./dbConnection.js');
-const users = {};
-// Create an event listener
+let userInfo;
+// var dbConnectionRef
+// dbConnection.then((db) => {
+    //     dbConnectionRef = db
+    // });
+    
+    
+// Middleware for parsing json in body payload
+app.use(bodyParser.json());
+// Add routes
+app.use("/chats", chatRouter);
+// Set the express.static middleware
+app.use(express.static(__dirname + "/client"));
+
 // When user connects
-socket.on("connection", (socket) => {
+socket.on("connection", (socketConn) => {
     console.log("User Connected!!"); // Prints when a new connection made
     // When user disconnects
-    socket.on('disconnect', () => {
+    socketConn.on('disconnect', () => {
         console.log("User Disconnected!!");
     });
 
-    /* socket.on('new-user', name => {
-    console.log("file: index.js ~ line 37 ~ socket.on ~ name", name)
-        users[socket.id] = name;
-        socket.broadcast.emit('user-connected', name);
-    }); */
+    socketConn.on('new-user', _userInfo => {
+        console.trace(_userInfo)
+        userInfo = _userInfo;
+        const usrName = userInfo.userName;
+        
+        console.trace(usrName)
+        socketConn.emit('user-connected', userInfo)
+    });
 
     // When someone types something
-    socket.on('typing', (data) => {
+    socketConn.on('typing', (data) => {
         // broadcasting to everyone
-        socket.broadcast.emit('notifyTyping', {
+        socketConn.broadcast.emit('notifyTyping', {
             user: data.user,
             message: data.message,
         });
     });
 
     // When someone stops typing
-    socket.on('stopTyping', () => {
-        socket.broadcast.emit('notifyStopTyping');
+    socketConn.on('stopTyping', () => {
+        socketConn.broadcast.emit('notifyStopTyping');
     });
 
     // Someone sends message
-    socket.on('sendMessage', (msg) => {
-        console.log("Logging message: ", msg);
+    socketConn.on('sendMessage', (msg) => {
+        const msgJSON = JSON.parse(msg)
+        console.log("Logging message: ", msgJSON);
 
         // Broadcast message to everyone
-        socket.broadcast.emit('msgReceived', {
-            message: msg
-        });
+        socketConn.broadcast.emit('msgReceived', msgJSON);
 
         // Create chat thread in Mongo DB while broadcasting to everyone
         dbConnection.then((db) => {
             console.log("Db connection with the server....");
 
             let data = new Chat({
-                message: msg,
-                senderId: "Anonymous",
-                // userId: "Mythili",
+                message: msgJSON.message,
+                senderId: msgJSON.username,
+                sendDate: Date(msgJSON.sendDate),
             });
 
             data.save();
+            // db.save(msgJSON);
         });
-
+        // dbConnectionRef.save(msgJSON)
     });
 });
 
